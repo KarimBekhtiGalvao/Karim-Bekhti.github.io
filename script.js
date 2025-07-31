721ed2f70729870877f10d79fa2b957701bee6ec
KarimBekhtiGalvao <kbekhti2002@yahoo.fr>
2025-07-31 13:58:51 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', () => {
-  const toggle = document.getElementById('nav-toggle');
-  const sidebar = document.getElementById('sidebar');
-
-  toggle.addEventListener('click', () => {
-    sidebar.classList.toggle('open');
-  });
-});
